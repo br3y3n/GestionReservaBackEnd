0b74b50d4f15b211aa326b2bb31b0af7c79073b2
yesicazemanate <ymzemanate@soy.sena.edu.co>
2024-09-18 15:49:05 -0500

    cambios

--- src/controllers/reserva.js
+++ src/controllers/reserva.js
@@ -2,13 +2,9 @@ import Reserva from "../models/reserva.js";
 export const createReserva = async (req, res) => {
     try {
       const data = req.body;
- 
-      const reservas = await Reserva.find({ lugar: data.lugar });
-      console.log(reservas);
-      if (reservas){
-        console.log(true);
-        }
-  
+      console.log(data)
+      const nuevoLugar = await Reserva.create(data);
+      return res.status(201).json({ message: 'reserva creada con éxito', lugar: nuevoLugar });
     } catch (error) {
       console.error('Error al crear el reserva:', error);
       return res.status(500).json({ message: 'Error al crear el reserva' });
@@ -19,25 +15,7 @@ export const createReserva = async (req, res) => {
     const { id } = req.params;
   
     try {
-      const reserva = await Reserva.findById(id).populate('usuario').populate('lugar');
-  
-      if (reserva) {
-        return res.status(200).json({ reserva });
-      } else {
-        return res.status(404).json({ message: 'reserva no encontrada' });
-      }
-    } catch (error) {
-      console.error('Error al obtener la reserva por ID:', error);
-      return res.status(500).json({ message: 'Error al obtener la reserva' });
-    }
-  };
-
-
-  export const getReservaByIdUser = async (req, res) => {
-    const { id } = req.params;
-  
-    try {
-      const reserva = await Reserva.findOne({usuario:id}).populate('usuario').populate('lugar');
+      const reserva = await Reserva.findById(id);
   
       if (reserva) {
         return res.status(200).json({ reserva });
@@ -57,7 +35,7 @@ export const createReserva = async (req, res) => {
       const reserva = await Reserva.find().populate('usuario').populate('lugar');
   
       if (reserva) {
-        return res.status(200).json(reserva);
+        return res.status(200).json({ reserva });
       } else {
         return res.status(404).json({ message: 'No se encontraron reserva' });
       }
