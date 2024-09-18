03a31c65a24b0269332612183f90601ea460ac66
Juan Vidal <juanmvg2003@gmail.com>
2024-09-18 13:36:44 -0500

    auth module - register

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 import express from 'express'
+import cors from "cors"
 import { config } from 'dotenv'
-import routerLugares from './src/routes/lugares.js'
 import { dbConnection } from './src/DB/dbConnection.js'
+
+
+
 const app = express()
 app.use(express.json())
 config()
 dbConnection()
+
+
+
+import routerLugares from './src/routes/lugares.js'
 app.use('/lugares', routerLugares)
+
+import authRouter from './src/routes/auth.routes.js';
+app.use("/api", authRouter);
+
+
+
 app.listen(process.env.PORT, ()=>{
     console.log("servidor funcionando correctamente")
 })
